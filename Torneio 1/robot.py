c1ada8ffbd977aae170f8e74a1a942ba0d92f1ab
z8Tygas <61952605+z8Tygas@users.noreply.github.com>
2022-02-19 20:44:12 +0000

    Update robot.py

--- Torneio 1/robot.py
+++ Torneio 1/robot.py
@@ -1,3 +1,19 @@
+'''
+Neste problema prentede-se que implemente uma função que calcula o rectângulo onde se movimenta um robot.
+
+Inicialmente o robot encontra-se na posição (0,0) virado para cima e irá receber uma sequência de comandos numa string.
+Existem quatro tipos de comandos que o robot reconhece:
+  'A' - avançar na direcção para o qual está virado
+  'E' - virar-se 90º para a esquerda
+  'D' - virar-se 90º para a direita 
+  'H' - parar e regressar à posição inicial virado para cima
+  
+Quando o robot recebe o comando 'H' devem ser guardadas as 4 coordenadas (minímo no eixo dos X, mínimo no eixo dos Y, máximo no eixo dos X, máximo no eixo dos Y) que definem o rectângulo 
+onde se movimentou desde o início da sequência de comandos ou desde o último comando 'H'.
+
+A função deve retornar a lista de todas os rectangulos (tuplos com 4 inteiros)
+'''
+
 def robot(comandos):
     rect = []
     mx, my, Mx, My, x, y = 0,0,0,0,0,0
